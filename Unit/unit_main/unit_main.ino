fe949ef6e82a94e629dbcbaf6625d0bd74b6ad7b
ColdeZhang <coldezhang@foxmail.com>
2020-09-02 21:51:03 +0800

    Update unit_main.ino

--- Unit/unit_main/unit_main.ino
+++ Unit/unit_main/unit_main.ino
@@ -1,6 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+===========================================
+         ____  ____  _   _ ____  
+        |  _ \/ ___|| | | / ___| 
+        | | | \___ \| |_| \___ \ 
+        | |_| |___) |  _  |___) |
+        |____/|____/|_| |_|____/ 
 
+      Deer  Smart   Home    System
+
+@Author:
+    Unlimited_Deer_
+
+@Type
+    Unit main script
+
+@Description:
+        Unit's source code of DSHS, 
+    running on Mega 2560. Using SPI
+    protocol to communicate with Hosts.
+===========================================
+*/
+
+#include <SPI.h>
+
+void setup() {
+  
+  // have to send on master in, *slave out*
+  pinMode(MISO, OUTPUT);
+
+  // turn on SPI in slave mode
+  SPCR |= _BV(SPE);
+  
 }
 
 void loop() {
