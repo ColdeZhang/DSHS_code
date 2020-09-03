50ed5e4a5fbbf69e32b2078b8adf440ea9b09b24
ColdeZhang <coldezhang@foxmail.com>
2020-09-03 11:13:32 +0800

    Update SPI.py

--- host/SPI.py
+++ host/SPI.py
@@ -1,3 +1,26 @@
+"""
+===========================================
+         ____  ____  _   _ ____  
+        |  _ \/ ___|| | | / ___| 
+        | | | \___ \| |_| \___ \ 
+        | |_| |___) |  _  |___) |
+        |____/|____/|_| |_|____/ 
+
+      Deer  Smart   Home    System
+
+@Author:
+    Unlimited_Deer_
+
+@Type
+    SPI protocol script
+
+@Description:
+        This script controlling SPI protocol
+    to communicate with Hosts.
+     
+===========================================
+"""
+
 import spidev
 
 def spiInit():
@@ -5,4 +28,24 @@ def spiInit():
     spi = spidev.SpiDev()
     spi.open(0, 0)
     spi.max_speed_hz = 7800000
+
+def transfer(jsonData):
+    '''Execute spi transfer and return a json from units (slave devices)'''
+    bytesData = json2bytes(jsonData)
+    rcvd = spi.xfer(bytesData)
+    jsonData = bytes2json(rcvd)
+    return jsonData
+
+
+def json2bytes(jsonData):
+    '''Convert json data to bytes data.'''
+    bytesData = null
+    return bytesData
+
+def bytes2json(bytesData: list):
+    '''Convert bytes data to json data'''
+    jsonData = null
+    return jsonData
+
+
     
